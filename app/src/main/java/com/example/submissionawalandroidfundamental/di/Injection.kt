e308c4645382e12f0197bd8d191e7729d239f075
Dimas Fahrul <dfpa.05@gmail.com>
2024-10-16 00:43:47 +0700

    delete: app executor

--- app/src/main/java/com/example/submissionawalandroidfundamental/di/Injection.kt
+++ app/src/main/java/com/example/submissionawalandroidfundamental/di/Injection.kt
@@ -4,14 +4,12 @@ import android.content.Context
 import com.example.submissionawalandroidfundamental.data.EventRepository
 import com.example.submissionawalandroidfundamental.data.local.room.EventDatabase
 import com.example.submissionawalandroidfundamental.data.remote.retrofit.ApiConfig
-import com.example.submissionawalandroidfundamental.utils.AppExecutors
 
 object Injection {
     fun provideRepository(context: Context): EventRepository {
         val apiService = ApiConfig.getApiService()
         val database = EventDatabase.getInstance(context)
         val dao = database.eventDao()
-        val appExecutors = AppExecutors()
-        return EventRepository.getInstance(apiService, dao, appExecutors)
+        return EventRepository.getInstance(apiService, dao)
     }
 }
